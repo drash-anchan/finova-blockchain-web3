360079a59bb2d0291c742b8df57eaa7acd432fb8
Drash Anchan <myselfdrash@gmail.com>
2026-01-09 22:24:16 +0530

    Implement Block class with hash calculation

---
+++ block.py
@@ -0,0 +1,22 @@
+import hashlib
+import json
+
+class Block:
+    def __init__(self, index, timestamp, transactions, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+        self.nonce = 0
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_data = json.dumps({
+            "index": self.index,
+            "timestamp": self.timestamp,
+            "transactions": self.transactions,
+            "previous_hash": self.previous_hash,
+            "nonce": self.nonce
+        }, sort_keys=True)
+
+        return hashlib.sha256(block_data.encode()).hexdigest()
